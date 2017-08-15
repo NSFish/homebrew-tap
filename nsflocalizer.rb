298fddf780c9841593f92a709ffa24526e3ec6cb
乐星宇 <lexingyu@MacBook-Pro.local>
2017-08-15 15:11:32 +0800

    更新formula

--- nsflocalizer.rb
+++ nsflocalizer.rb
@@ -2,7 +2,7 @@ class Nsflocalizer < Formula
   desc ".strings文件和Excel语言包之间的双向同步工具"
   homepage "https://github.com/NSFish/NSFLocalizationSynchronizer"
   url "https://github.com/NSFish/NSFLocalizationSynchronizer/releases/download/1.0/nsflocalizer"
-  sha256 "c2c384880585ff7290365909852165eb21af81c7d31f5fcebaf3688df1abf1f7"
+  sha256 "3d7b240c24ddf2396dc5d194a2c29f1806744da68e9292655cb6f46bf6a09d13"
 
   def install
     bin.install "nsflocalizer"
