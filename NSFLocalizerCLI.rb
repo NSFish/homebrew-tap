5bf9ea995f91631455fe8f574a2d7c14dd334225
乐星宇 <lexingyu@MacBook-Pro.local>
2017-08-15 14:40:11 +0800

    更新formula

--- NSFLocalizerCLI.rb
+++ NSFLocalizerCLI.rb
@@ -1,10 +1,10 @@
-class Nsflocalizationsynchronizer < Formula
+class NSFLocalizerCLI.rb < Formula
   desc ".strings文件和Excel语言包之间的双向同步工具"
   homepage "https://github.com/NSFish/NSFLocalizationSynchronizer"
-  url "https://github.com/NSFish/NSFLocalizationSynchronizer/releases/download/1.0/NSFLocalizationSynchronizerCLI"
+  url "https://github.com/NSFish/NSFLocalizationSynchronizer/releases/download/1.0/NSFLocalizerCLI"
   sha256 "614f059c1e3b874061805b6299880bcbde0fed5dcf8906e9d71af8a6e7645381"
 
   def install
-    bin.install "NSFLocalizationSynchronizerCLI"
+    bin.install "NSFLocalizerCLI"
   end
 end
