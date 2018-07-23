454e55d488134a1a27846dd1cd7fc761125bc942
NSFish <lxy910428@gmail.com>
2018-07-24 06:04:06 +0800

    更新 optool 的 formula

--- optool.rb
+++ optool.rb
@@ -1,8 +1,8 @@
 class Optool < Formula
   desc "Command Line Tool for interacting with MachO binaries on OSX/iOS"
   homepage "https://github.com/NSFish/optool"
-  url "https://github.com/NSFish/optool/releases/tag/0.1"
-  sha256 "3f91163b032d9414608519770cc3838885e26b17019c37cb15eb9155c78b5dfc"
+  url "https://github.com/NSFish/optool/releases/tag/0.2"
+  sha256 "9dbfb16b01d7fe5c074e7a923830fc492e9abef2983fd5bd72d286511476e0de"
 
   def install
     bin.install "optool"
