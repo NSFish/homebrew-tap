fd609f1808b0f1c82f9bb96feb01eff074651209
NSFish <lxy910428@gmail.com>
2018-08-24 17:28:07 +0800

    Delete personal-scripts

--- personal-scripts.rb
+++
@@ -1,9 +0,0 @@
-class PersonalScripts < Formula
-  desc "Personal Scripts"
-  homepage "https://github.com/NSFish/PersonalScripts"
-  head 'https://github.com/NSFish/PersonalScripts.git'
-
-  def install
-    bin.install "ramdisk.sh"
-  end
-end
