53f9629741ce9d5f341fc488afe93f8718235d17
NSFish <lxy910428@gmail.com>
2018-08-24 16:53:42 +0800

    Add personal scripts

---
+++ personal_scripts.rb
@@ -0,0 +1,9 @@
+class PersonalScripts < Formula
+  desc "Personal Scripts"
+  homepage "https://github.com/NSFish/PersonalScripts"
+  head 'https://github.com/NSFish/PersonalScripts.git'
+
+  def install
+    bin.install "ramdisk"
+  end
+end
