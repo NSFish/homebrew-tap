041106133dda3056a51a765cae0b7bb94c31e09c
NSFish <lxy910428@gmail.com>
2020-05-13 04:06:17 +0800

    update nsf-mkv-master

--- nsf-mkv-master.rb
+++ nsf-mkv-master.rb
@@ -1,8 +1,8 @@
 class NsfMkvMaster < Formula
     desc "Wrapping mkvtoolnix for easier use"
     homepage "https://github.com/NSFish/nsf-mkv-master"
-    url "https://github.com/NSFish/nsf-mkv-master/releases/download/1.1/nsf-mkv-master"
-    sha256 "43d14cd054009981a39cd956bbb5cc9178af0ff947fabc5db4c1f85fc57efac1"
+    url "https://github.com/NSFish/nsf-mkv-master/releases/download/1.2/nsf-mkv-master"
+    sha256 "220ee7a514aa9c4c101d49033a8b1486d975b49e9f0389e86f5c254977dce9f0"
   
     def install
       bin.install "nsf-mkv-master"
