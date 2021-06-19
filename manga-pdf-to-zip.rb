de7fbae3aef5246d3a74856c42dab5108ba1d9b3
NSFish <lxy910428@gmail.com>
2021-06-19 20:10:45 +0800

    update

--- manga-pdf-to-zip.rb
+++ manga-pdf-to-zip.rb
@@ -1,10 +1,10 @@
 class MangaPdfToZip < Formula
     desc "extract images from pdf and then zip them up."
     homepage "https://github.com/NSFish/MangaPDF2Zip"
-    url "https://github.com/NSFish/MangaPDF2Zip/releases/download/1.0/MangaPDF2Zip"
-    sha256 "0bb127f36e23ad0a1ed2e0f7a1e96a6f44bff0a4c33a6c325ea7053b0eedea83"
+    url "https://github.com/NSFish/MangaPDF2Zip/releases/download/1.0/manga-pdf-to-zip"
+    sha256 "d22c3df735a04c88f16cd9ed6c5519d06c191befda3f2f7fdc0f4427a98f54cd"
   
     def install
-      bin.install "MangaPDF2Zip"
+      bin.install "manga-pdf-to-zip"
     end
   end
