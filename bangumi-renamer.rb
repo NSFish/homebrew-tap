624ddff5e145444f74cd0877946c403c986b1d68
NSFish <lxy910428@gmail.com>
2021-09-19 17:48:16 +0800

    update

--- bangumi-renamer.rb
+++ bangumi-renamer.rb
@@ -1,8 +1,8 @@
 class BangumiRenamer < Formula
     desc "Rename downloaded bangumi series according to infomation from places like wikipedia"
     homepage "https://github.com/NSFish/BangumiRenamer"
-    url "https://github.com/NSFish/BangumiRenamer/releases/download/1.5.0/bangumi-renamer"
-    sha256 "fe1268034a6502f183e0b8547188ee1c431af56b98009f8952b474cd73086b5d"
+    url "https://github.com/NSFish/BangumiRenamer/releases/download/1.5.1/bangumi-renamer"
+    sha256 "0ce1c51e950316aea1231cca2e4478799c0c8e0c0959316ebd8f410cc12240d0"
   
     def install
       bin.install "bangumi-renamer"
