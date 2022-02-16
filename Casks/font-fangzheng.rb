fe7de520067a3a721a56ef559dccf98c9d4a55bc
NSFish <lxy910428@gmail.com>
2022-02-17 03:52:34 +0800

    update

--- Casks/font-fangzheng.rb
+++ Casks/font-fangzheng.rb
@@ -1,9 +1,9 @@
-cask "font-3270-nerd-font" do
+cask "font-fangzheng" do
     version "1.0"
     sha256 "a50f7b0768ace402a2fc40fd44033ce3ff863fedfdc359dab9867dd5e82d2a24"
   
     url "file:///Users/nsfish/Dropbox/Backup/Fonts.zip"
-    name "FangZheng"
+    name "font-fangzheng"
     homepage "https://www.foundertype.com/index.php/Trade/myorder.html"
   
     font "FZBIAOYSJW.TTF"
