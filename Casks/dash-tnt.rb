dccdba0b126abf7fd175859b28a3d6c530128536
NSFish <lxy910428@gmail.com>
2022-02-17 04:05:02 +0800

    update

--- Casks/dash-tnt.rb
+++ Casks/dash-tnt.rb
@@ -1,6 +1,5 @@
 cask "dash-tnt" do
     version "6.2.2"
-    sha256 "d183e04381df8e8eab0a41a9d3a5e9405350de627471fb0d86dc0042a7a38825"
 
     url "file:///Volumes/%E6%B5%B7%E8%B1%9A/%E5%A4%87%E4%BB%BD/%E8%BD%AF%E4%BB%B6/Dash%20#{version}%20%5BTNT%5D.dmg"
     name "Dash"
