ce42f24eb4aec237c364250ab0ba7de6a9226a6d
NSFish <lxy910428@gmail.com>
2022-02-17 02:59:55 +0800

    update

--- xiaomi-cloud.rb
+++ xiaomi-cloud.rb
@@ -1,9 +1,11 @@
-cask "xiaomicloud" do
+cask "xiaomi-cloud" do
     version "1.2.6"
+    sha256 "20203a2f42d82c3e38f7bf5121c6fed2af8e4bd99004c871cb5bbd60d900734d"
+
+    url "https://cdn.cnbj1.fds.api.mi-img.com/archive/update-server/public/darwin/x64/XiaomiCloud-1.2.6.dmg"
+    name "xiaomi-cloud"
     desc "小米云服务 for mac"
     homepage "https://i.mi.com/static2?filename=MicloudWebStatic/res/home/mi-lab.htm&locale=zh_CN"
-    url "https://cdn.cnbj1.fds.api.mi-img.com/archive/update-server/public/darwin/x64/XiaomiCloud-1.2.6.dmg"
-    sha256 "20203a2f42d82c3e38f7bf5121c6fed2af8e4bd99004c871cb5bbd60d900734d"
 
     app "小米云服务.app"
 end
