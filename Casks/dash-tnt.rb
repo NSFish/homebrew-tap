f5684fdcf84fdb785bd49725a4b27cce35fa58d2
NSFish <lxy910428@gmail.com>
2022-02-17 04:20:29 +0800

    update

--- Casks/dash-tnt.rb
+++ Casks/dash-tnt.rb
@@ -1,10 +1,11 @@
 cask "dash-tnt" do
     version "6.2.2"
 
-    url "file:///Volumes/%E6%B5%B7%E8%B1%9A/%E5%A4%87%E4%BB%BD/%E8%BD%AF%E4%BB%B6/Dash%20#{version}%20%5BTNT%5D.dmg"
+    url "file:///Volumes/%E6%B5%B7%E8%B1%9A/%E5%A4%87%E4%BB%BD/%E8%BD%AF%E4%BB%B6/Dash_6_2_2_TNT.dmg"
     name "Dash"
     desc "Dash[TNT]"
     homepage "https://appstorrent.ru/196-dash.html"
+    container nested: "Manual install/Dash 6.2.2 [TNT].dmg"
 
     app "Dash.app"
 end
