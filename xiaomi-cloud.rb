01b0af2d1425763d4be8ff0beaa9d0dd7b2f05c3
NSFish <lxy910428@gmail.com>
2022-02-17 02:54:03 +0800

    update

--- xiaomi-cloud.rb
+++ xiaomi-cloud.rb
@@ -1,4 +1,5 @@
-class XiaomiCloud < Formula
+cask "XiaomiCloud" do
+    version "1.2.6"
     desc "小米云服务 for mac"
     homepage "https://i.mi.com/static2?filename=MicloudWebStatic/res/home/mi-lab.htm&locale=zh_CN"
     url "https://cdn.cnbj1.fds.api.mi-img.com/archive/update-server/public/darwin/x64/XiaomiCloud-1.2.6.dmg"
