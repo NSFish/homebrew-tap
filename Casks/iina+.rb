a00260ba2f10e333a3ecdc39da077e7fa9cbb1e4
NSFish <lxy910428@gmail.com>
2022-02-17 08:05:15 +0800

    update

---
+++ Casks/iina+.rb
@@ -0,0 +1,11 @@
+cask "iina+" do
+    version "5.5.0"
+    sha256 "0d8d8b1e9572f24cc0d2d3480c0ebde359f666dfee80f149910d6544f8171cfc"
+
+    url "file:///Volumes/%E6%B5%B7%E8%B1%9A/%E5%A4%87%E4%BB%BD/%E8%BD%AF%E4%BB%B6/IINA+.app.zip"
+    name "IINA+"
+    desc "IINA Plus"
+    homepage "https://github.com/xjbeta/iina-plus/releases"
+
+    app "IINA+.app"
+end
