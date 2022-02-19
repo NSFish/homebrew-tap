501f14e5c5ce02a0841ff8d891f31f579ecf8efb
NSFish <lxy910428@gmail.com>
2022-02-19 10:39:28 +0800

    update

---
+++ Casks/chengtong-netdisk.rb
@@ -0,0 +1,10 @@
+cask "chengtong-netdisk" do
+    version "73.0.3683.86"
+    sha256 "c56b48df5841832978e57193cd1bde35fe82934081e8fff1f95b5ea243dc76b3"
+
+    url "file:///Users/nsfish/Downloads/a/ctfile.dmg"
+    name "城通网盘"
+    homepage "https://github.com/NSFish/Piracy"
+
+    app "城通网盘.app"
+end
