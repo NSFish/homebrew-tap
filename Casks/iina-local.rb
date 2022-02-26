37941de80d547178e9f055b876d4dc01682ac856
NSFish <lxy910428@gmail.com>
2022-02-26 22:01:26 +0800

    update

---
+++ Casks/iina-local.rb
@@ -0,0 +1,10 @@
+cask "iina-local" do
+    version "1.2.0-Danmaku"
+    sha256 "d5eb39ebc99c9154d5fc87e0fa1ebd59556a163df7f2aa46bdc1902d289249e6"
+
+    url "file:///Volumes/%E6%B5%B7%E8%B1%9A/%E5%A4%87%E4%BB%BD/%E5%AE%89%E8%A3%85%E5%8C%85/IINA.1.2.0-Danmaku.dmg"
+    name "IINA"
+    homepage "https://github.com/xjbeta/iina-plus/releases"
+
+    app "IINA.app"
+end
--- Casks/iina-plus-local.rb
+++ Casks/iina-plus-local.rb
@@ -1,8 +1,8 @@
 cask "iina-plus-local" do
     version "0.5.25"
-    sha256 "0d8d8b1e9572f24cc0d2d3480c0ebde359f666dfee80f149910d6544f8171cfc"
+    sha256 "9b1ee3c6a44574346760d86779aec93c94f33a8081b5d43295e90bd0a3f1cc89"
 
-    url "file:///Volumes/%E6%B5%B7%E8%B1%9A/%E5%A4%87%E4%BB%BD/%E5%AE%89%E8%A3%85%E5%8C%85/IINA+.zip"
+    url "file:///Volumes/%E6%B5%B7%E8%B1%9A/%E5%A4%87%E4%BB%BD/%E5%AE%89%E8%A3%85%E5%8C%85/IINA+.app.zip"
     name "IINA+"
     homepage "https://github.com/xjbeta/iina-plus/releases"
 
