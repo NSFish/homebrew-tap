bf35db1a642e9300d36b3a4ebf9f411c7986f2f7
NSFish <lxy910428@gmail.com>
2022-03-01 18:53:44 +0800

    update

--- Casks/iina-plus-local.rb
+++ Casks/iina-plus-local.rb
@@ -1,6 +1,6 @@
 cask "iina-plus-local" do
     version "0.5.25"
-    sha256 "9b1ee3c6a44574346760d86779aec93c94f33a8081b5d43295e90bd0a3f1cc89"
+    sha256 "dd62a3e62c6f473ba91a3421caeeadc3f620993049d4bf7167e356324bf41c01"
 
     url "file:///Volumes/%E6%B5%B7%E8%B1%9A/%E5%A4%87%E4%BB%BD/%E5%AE%89%E8%A3%85%E5%8C%85/IINA+.app.zip"
     name "IINA+"
